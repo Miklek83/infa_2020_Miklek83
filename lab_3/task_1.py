9d8115d7da43417690eed23e646bdca7bb638045
Miklek83 <miklek83@gmail.com>
2020-11-24 20:37:07 +0300

    Добавлены брови

--- lab_3/task_1.py
+++ lab_3/task_1.py
@@ -14,7 +14,7 @@ FPS = 30
 
 def main ():
     
-    #Задаем размеры и координаты смайлика
+    #Задаем размеры и координаты центра смайлика
     x,y=200,200
     width_smile,height_smile=350,350
     
@@ -45,7 +45,9 @@ def draw_smile (x,y,width,height):
 
     draw_face (x,y,width,height)
     
-    draw_eyes (x,y,width,height)  
+    draw_eyes (x,y,width,height)
+
+    draw_eyebrow (x,y,width,height) 
 
 
 def draw_face (x,y,width,height):
@@ -64,7 +66,7 @@ def draw_eyes(x,y,width,height):
     circle(screen, BLACK, (x_left_eyes,y_left_eyes), width/2*0.19, 1)
     circle(screen, RED, (x_left_eyes, y_left_eyes), width/2*0.19-1, 0)
     circle(screen, BLACK, (x_left_eyes, y_left_eyes),width/2*0.08, 0)
-    line(screen, BLACK, (100,120), (175,135), 15)
+    #line(screen, BLACK, (100,120), (175,135), 15)
 
 
     # Правый глаз
@@ -72,6 +74,21 @@ def draw_eyes(x,y,width,height):
     circle(screen, RED, (x_right_eyes, y_right_eyes), width/2*0.14-1, 0)
     circle(screen, BLACK, (x_right_eyes, y_right_eyes), width/2*0.06, 0)
 
+
+def draw_eyebrow (x,y,width,height):
+    x1_left_eyes=x-width/2*0.8
+    y1_left_eyes=y-height/2*0.6
+    x2_left_eyes=x-width/2*0.1
+    y2_left_eyes=y-height/2*0.3
+
+    x1_right_eyes=x+width/2*0.8
+    y1_right_eyes=y-height/2*0.6
+    x2_right_eyes=x+width/2*0.1
+    y2_right_eyes=y-height/2*0.3
+
+    
+    line(screen, BLACK,(x1_left_eyes,y1_left_eyes),(x2_left_eyes,y2_left_eyes), int(width/2*0.08))
+    line(screen, BLACK,(x1_right_eyes,y1_right_eyes),(x2_right_eyes,y2_right_eyes), int(width/2*0.08))
 
 pygame.init()
 screen = pygame.display.set_mode((400, 400))
